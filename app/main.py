1cba31ea725c7f07d5ded38fd6fce5c38922b53f
Joaol324j <joaol324j@gmail.com>
2025-03-27 08:16:24 -0300

    Add Jinja2

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 from app.routes import menu_route, promotion_route, order_route,comment_route, user_route, reset_route
 from app.configs.db import Base, engine
 
@@ -6,9 +8,13 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Pizza_API")
 
+templates = Jinja2Templates(directory="app/templates")
+
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 app.include_router(menu_route.router)
 app.include_router(promotion_route.router)
